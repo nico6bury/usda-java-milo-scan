1851351196e12105e0ee36a003d8c4e8c542ea2d
Nicholas Sixbury <Nicholas.Sixbury@usda.gov>
2024-06-18 14:54:03 -0500

    Changed output table to better fit milo model

--- src/View/MainWindow.java
+++ src/View/MainWindow.java
@@ -567,18 +567,16 @@ public class MainWindow extends javax.swing.JFrame {
 
             },
             new String [] {
-                "FileID", "TH", "left cnt", "rght cnt", "left L*", "rght L*", "Avg L*", "left %A", "rght %A", "Avg %A", "Flag"
+                "FileID", "GridIdx", "EndospermArea", "Endosperm%Area", "KernelArea"
             }
         ) {
-            @SuppressWarnings("rawtypes")
             Class[] types = new Class [] {
-                java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class
+                java.lang.String.class, java.lang.Integer.class, java.lang.Double.class, java.lang.Double.class, java.lang.Double.class
             };
             boolean[] canEdit = new boolean [] {
-                false, false, false, false, false, false, false, false, false, false, false
+                false, false, false, false, false
             };
 
-            @SuppressWarnings({ "rawtypes", "unchecked" })
             public Class getColumnClass(int columnIndex) {
                 return types [columnIndex];
             }
