88e08649594aeb2caff147f9f3fb2cd0f8c68903
Nicholas Sixbury <Nicholas.Sixbury@usda.gov>
2024-06-26 15:20:18 -0500

    Suppressed warnings

--- src/View/MainWindow.java
+++ src/View/MainWindow.java
@@ -570,6 +570,7 @@ public class MainWindow extends javax.swing.JFrame {
                 "FileID", "GridIdx", "Area1", "Area2", "%Area2"
             }
         ) {
+            @SuppressWarnings("rawtypes")
             Class[] types = new Class [] {
                 java.lang.String.class, java.lang.Integer.class, java.lang.Double.class, java.lang.Double.class, java.lang.Double.class
             };
@@ -577,6 +578,7 @@ public class MainWindow extends javax.swing.JFrame {
                 false, false, false, false, false
             };
 
+            @SuppressWarnings({ "rawtypes", "unchecked" })
             public Class getColumnClass(int columnIndex) {
                 return types [columnIndex];
             }
